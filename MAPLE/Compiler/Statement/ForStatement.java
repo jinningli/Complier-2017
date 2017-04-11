018769a4736de800bcaf55158482985e47473394
黎金宁 <lijinning@sjtu.edu.cn>
2017-04-11 22:16:57 +0800

    test3

--- MAPLE/Compiler/Statement/ForStatement.java
+++ MAPLE/Compiler/Statement/ForStatement.java
@@ -35,7 +35,7 @@ public class ForStatement extends Statement{
        for(int i = 0; i < 3; i ++){
            type[i] = expr[i].getretype();
         }
-        if(!(type[0] instanceof AssignExpr)){ ////三个参数可以省略吗？？？
+        if(!(type[0] instanceof AssignExpr)){ ////can three expr empty?
            throw new TypeNotMatch();
         }
         if(!(type[1] instanceof BoolType)){
