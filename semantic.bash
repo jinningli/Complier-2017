17a696f62c8028d410e7dfe4300ef46119bb820b
黎金宁 <lijinning@sjtu.edu.cn>
2017-05-17 10:28:38 +0800

    Maple: Pretest trying

--- semantic.bash
+++ semantic.bash
@@ -6,4 +6,4 @@
 set -e
 cd "$(dirname "$0")"
 #cat > program.txt   # save everything in stdin to program.txt
-java -cp ./lib/*:./bin Compiler.FrontEnd.Main
+java -cp ./cn/chips/MAPLE/lib/*:./bin cn.chips.MAPLE.compiler.Main
