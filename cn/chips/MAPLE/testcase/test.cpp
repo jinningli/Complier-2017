dc687fce230cb86a6cba85fb4468362158302dcd
黎金宁 <lijinning@sjtu.edu.cn>
2017-06-10 13:37:47 +0800

    标准输入。。。

--- cn/chips/MAPLE/testcase/test.cpp
+++ cn/chips/MAPLE/testcase/test.cpp
@@ -1,25 +1,5 @@
-int add(int a, int b) {
-    return a + b;
-}
-
-
-
-int n = 1 + 3;
-
-int main() {
-    int n = 100;
-    int s = 0;
-    int i;
-    int[] a = new int[n];
-
-    for (i = 0; i < n; i++)
-        a[i] = i;
-
-    for (i = 0; i < n; i++) {
-        println(toString(a[i]));
-        s = add(s, i);
-    }
-
-    println(toString(s));
-    return 0;
+int main()
+{
+  println("Hello, world!");
+  return 0;
 }
